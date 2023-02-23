07793e386ceb963ad71c6972a036eaf8d8fe3083
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-02-23 14:00:45 -0800

    Write a function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
+ * Return: void
  */
 void print_numbers(void)
 {
-
 	int i;
 
 	while (i < 10)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 		i++;
 	}
 	_putchar('\n');
