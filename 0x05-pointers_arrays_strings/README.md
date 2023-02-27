6ee0188d492c71e79eb1412e29ea8287e73dae76
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-02-27 08:53:57 -0800

    Kamohelo/ Created a main.h to store prototypes

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning about pointers, Arrays and Strings
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Description: Header file contains the prototypes for all functions
+ */
+
+#include <stdlib.h>
+int _putchar(char c);
+
+#endif
