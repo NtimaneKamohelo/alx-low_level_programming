532a6017d5410bd9c5fee2d5873eb183400b58c0
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-03-01 13:23:43 -0800

    Kamohelo/Concatinate pointers

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: mai.h
+ * Desc: Header file containing prototypes for all functions used in the 0x06. C - More pointers, arrays and strings
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
