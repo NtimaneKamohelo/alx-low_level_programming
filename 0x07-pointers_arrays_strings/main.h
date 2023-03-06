a2edcef643ff89a037f45a686422e7ba5bba209d
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-03-06 09:50:50 -0800

    Kamohelo/ function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: MAIN.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+#endif
