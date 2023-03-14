42b89b09e891c35e161f1e997d8777168c15d323
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-03-14 16:35:19 -0700

    error: ISO C90 forbids mixed declarations and code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,8 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *result;
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
 
 	if (s1 == NULL)
 	{
@@ -24,8 +26,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
 
 	result = malloc((len1 + len2 + 1) * sizeof(char));
 	if (result == NULL)
