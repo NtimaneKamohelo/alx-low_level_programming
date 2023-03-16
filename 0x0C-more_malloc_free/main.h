fdad9ea48720fa1ca2d9025894f007cf38fceff6
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-03-16 14:37:16 -0700

    function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0C-more_malloc_free directory.
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
