87c3393c217da35e6f7c862dee66a7e1d5ec890c
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-03-21 02:26:06 -0700

    Kamohelo/Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type struct dog with the following elements
+ * @name: type char *
+ * @age: type float
+ * @owner: type char *
+ *
+ * Description: a new type struct dog with the following elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
