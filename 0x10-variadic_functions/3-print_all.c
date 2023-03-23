7d067a14a465a99d241f71adb10dcf53a3d0fa74
Kamohelo Ntimane <ntimanevincent8@gmail.com>
2023-03-23 09:22:06 -0700

    added the prototype in print_all file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+void print_all(const char * const format, ...);
+
 /**
  * print_all - Prints anything, followed by a new line.
  * @format: A string of characters representing the argument types.
